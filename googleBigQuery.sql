bd4f076c42ddbcb95aef074b5a1fb8bfc6eaa828
Sobana <123670324+jaysobana@users.noreply.github.com>
2023-03-08 23:27:13 -0800

    Update googleBigQuery.sql

--- googleBigQuery.sql
+++ googleBigQuery.sql
@@ -25,23 +25,55 @@ ON
   activity.ActivityDate = steps.ActivityDay
   
 --To find the start and end date of each activity
-
+--Table: daily_activity
 SELECT
   MIN(ActivityDate) as start_date,
   MAX(ActivityDate) as end_date
 FROM
   fitbit-data-00.bellabeat.daily_activity
-
+--Table: sleep_day
 SELECT
   MIN(SleepDay) as start_date,
   MAX(SleepDay) as end_date
 FROM
   fitbit-data-00.bellabeat.sleep_day
-
+--Table: weight_log
 SELECT
   MIN(Date) as start_date,
   MAX(Date) as end_date
 FROM
   fitbit-data-00.bellabeat.weight_log
-
   
+To Find duplicates of each row
+--Table: daily_activity
+SELECT
+  Id,
+  ActivityDate,
+  Count(*) as num_of_ids
+FROM
+  fitbit-data-00.bellabeat.daily_activity
+GROUP BY
+  Id, ActivityDate
+Having num_of_ids >1
+
+--Table: sleep_day
+SELECT
+  Id,
+  SleepDay,
+  Count(*) as num_of_ids
+FROM
+  data-analytics-00.bellabeat.sleep_day
+GROUP BY
+  Id, SleepDay
+Having num_of_ids >1
+
+--Table: weight_log
+SELECT
+  Id,
+  Date,
+  Count(*) as num_of_ids
+FROM
+  data-analytics-00.bellabeat.weight_log
+GROUP BY
+  Id, Date
+Having num_of_ids >1
